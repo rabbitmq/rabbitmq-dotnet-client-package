c4caf382a8428e74b7f210eb945ced9ab30ff588
Joe Fitzgerald <joe.fitzgerald@emc.com>
2013-09-11 18:47:53 -0600

    Fixing Tool Path

--- src/buildalltopublic.bat
+++ src/buildalltopublic.bat
@@ -3,4 +3,4 @@ mkdir deploy
 ..\tools\NuGet.exe pack RabbitMQ.Client\pkg\RabbitMQ.Client.nuspec
 ..\tools\NuGet.exe pack RabbitMQ.ServiceModel\pkg\RabbitMQ.ServiceModel.nuspec
 for /f %%a in ('dir /b /s .\*.nupkg') do call move /Y %%a deploy
-for /f %%a in ('dir /b /s .\deploy\*.nupkg') do call ..\..\tools\NuGet.exe push %%a
+for /f %%a in ('dir /b /s .\deploy\*.nupkg') do call ..\tools\NuGet.exe push %%a
